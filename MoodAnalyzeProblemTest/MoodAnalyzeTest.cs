11bc6cd6d14f2c5ccfe13f21e0f0ec6d6dd8588c
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-05 15:36:15 +0530

    [Siddharth] Add:Ability to analyse & respond Happy or Sad Mood

--- MoodAnalyzeProblemTest/MoodAnalyzeTest.cs
+++ MoodAnalyzeProblemTest/MoodAnalyzeTest.cs
@@ -1,16 +1,23 @@
-namespace MoodAnalyzeProblemTest
+using MoodAnalyzerProblem;
+
+namespace MoodAnalyzerProblemTest
 {
-    public class Tests
+    public class MoodAnalyzerTest
     {
-        [SetUp]
-        public void Setup()
+        [Test] //TC1.1
+        public void GivenSadMood_WhenAnalyze_ShouldReturnSad()
         {
+            MoodAnalyzer analyzer = new MoodAnalyzer(); //Arrange
+            string result = analyzer.AnalyzeMood("I am in a Sad Mood"); //Act
+            Assert.AreEqual(result, "Sad");
         }
 
-        [Test]
-        public void Test1()
+        [Test] //TC1.2
+        public void GivenAnyMood_WhenAnalyze_ShouldReturnHappy()
         {
-            Assert.Pass();
+            MoodAnalyzer analyzer = new MoodAnalyzer(); //Arrange
+            string result = analyzer.AnalyzeMood("I am in a Any Mood"); //Act
+            Assert.AreEqual(result, "Happy");
         }
     }
 }
--- MoodAnalyzerProblem/MoodAnalyzer.cs
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -1,7 +1,16 @@
-﻿namespace MoodAnalyzerProblem
+﻿using MoodAnalyzerProblem;
+
+namespace MoodAnalyzerProblem
 {
     public class MoodAnalyzer
-    {
-
+    {       
+        public string AnalyzeMood(string message)
+        {
+            if (message.Contains("Sad"))
+                return "Sad";
+            else
+                return "Happy";
+        }
     }
 }
+          
